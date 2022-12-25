f83e449848771bb95f1c55d3004a1509cf507042
neiwan <neiwan@mail.ru>
2022-12-25 22:29:17 +0500

    1

---
+++ my_lib.py
@@ -0,0 +1,9 @@
+def bubbleSort(array):
+    length = len(array)
+    for i in range(length - 1):
+        for j in range(0, length - i - 1):
+            if array[j] > array[j + 1]:
+                temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+    return array
